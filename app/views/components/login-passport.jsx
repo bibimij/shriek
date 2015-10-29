25cb030f9966e457e8efb249427bc11964b20237
Alex <me@alexmil.ru>
2015-10-30 01:15:51 +0300

    Last russian localization fix

--- app/views/components/login-passport.jsx
+++ app/views/components/login-passport.jsx
@@ -6,12 +6,17 @@ var LoginPassport = function (socket) {
     render: function () {
       return (
         <div>
-          <h2>Please, specify your password</h2>
+          <h2>Придумайте пароль</h2>
           <div className="form__row">
-            <label className="form__label" htmlFor="inputPassword"><i className="fa fa-asterisk"></i></label>
-            <input className={this.props.classPass} onChange={this.props.handlePassword} type="password"id="inputPassword" placeholder="Password"/>
+            <label className="form__label" htmlFor="inputPassword">
+              <i className="fa fa-asterisk"></i>
+            </label>
+
+            <input className={this.props.classPass} onChange={this.props.handlePassword} type="password"
+              id="inputPassword" placeholder="Пароль" />
           </div>
-          <button className="btn" type="submit">Sign in</button>
+
+          <button className="btn" type="submit">Войти</button>
         </div>
       );
 
